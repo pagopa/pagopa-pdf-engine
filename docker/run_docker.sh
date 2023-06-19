9638fb9d958cc9c3c4f76450224b2c50f552b934
Alessio Cialini <acialini@external.nexigroup.com>
2023-06-19 11:05:38 +0200

    [PRDP-14] ci: Updated run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -50,20 +50,19 @@ docker build -t pagopa-pdf-engine ../
 docker run -p 80:80 -dit  --name="${stack_name}" pagopa-pdf-engine
 docker wait "${stack_name}"
 
-sleep 120s
 
 # waiting the containers
 printf 'Waiting for the service'
-#attempt_counter=0
-#max_attempts=80
-#until $(curl --output /dev/null --silent --head --fail http://localhost:80/info); do
-#    if [ ${attempt_counter} -eq ${max_attempts} ];then
-#      echo "Max attempts reached"
-#      exit 1
-#    fi
-#
-#    printf '.'
-#    attempt_counter=$((attempt_counter+1))
-#    sleep 5
-#done
+attempt_counter=0
+max_attempts=50
+until $(curl --output /dev/null --silent --head --fail http://localhost:80/info); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$((attempt_counter+1))
+    sleep 5
+done
 echo 'Service Started'
