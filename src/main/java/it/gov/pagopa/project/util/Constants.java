e1b946baca6b3c6c54fe23364576a9badb1c8735
giomella <gioele.mella.st@everis.nttdata.com>
2023-06-27 17:21:17 +0200

    fix path

--- src/main/java/it/gov/pagopa/project/util/Constants.java
+++ src/main/java/it/gov/pagopa/project/util/Constants.java
@@ -2,7 +2,7 @@ package it.gov.pagopa.project.util;
 
 public class Constants {
 
-    public static final String WORKING_DIRECTORY_FOLDER= "/workingDir";
+    public static final String WORKING_DIRECTORY_FOLDER= "/tmp/workingDir";
     public static final String UNZIPPED_FILES_FOLDER = "/unzipped";
     public static final String ZIP_FILE_NAME = "/input.zip";
 
