1fe6d683b26e1d333a9d4a2ebc4d0b362b83070a
giomella <gioele.mella.st@everis.nttdata.com>
2023-07-31 17:51:42 +0200

    [PRDP-85] added response body to info endpoint and updated unit tests

--- src/main/java/it/gov/pagopa/pdf/engine/Info.java
+++ src/main/java/it/gov/pagopa/pdf/engine/Info.java
@@ -4,8 +4,13 @@ import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import it.gov.pagopa.project.model.AppInfo;
 
+import java.io.InputStream;
 import java.util.Optional;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Azure Functions with Azure Http trigger.
@@ -24,7 +29,24 @@ public class Info {
 			authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
 			final ExecutionContext context) {
 
-		return request.createResponseBuilder(HttpStatus.OK).build();
+		return request.createResponseBuilder(HttpStatus.OK)
+                .body(getInfo(context.getLogger(), "/META-INF/maven/it.gov.pagopa.project/pdf-engine/pom.properties"))
+				.build();
 	}
-
+	public synchronized AppInfo getInfo(Logger logger, String path) {
+		String version = null;
+		String name = null;
+		try {
+			Properties properties = new Properties();
+			InputStream inputStream = getClass().getResourceAsStream(path);
+			if (inputStream != null) {
+				properties.load(inputStream);
+				version = properties.getProperty("version", null);
+				name = properties.getProperty("artifactId", null);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Impossible to retrieve information from pom.properties file.", e);
+		}
+		return AppInfo.builder().version(version).environment("azure-fn").name(name).build();
+	}
 }
