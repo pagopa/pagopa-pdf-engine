159963ef7834313c9b87021295d94cdde776f128
Alessio Cialini <63233981+alessio-cialini@users.noreply.github.com>
2023-08-01 07:40:32 +0200

    [PRDP-64] feat: Introducing agents (#21)
    * [PRDP-64] feat: Updated chart, refactored package, introducing agents download inside Docker file, updated values
    
    * Bump to version 1.4.2-1-PRDP-64-feat-add-elk-apm-configurations [skip ci]
    
    * Bump to version 1.4.3-PRDP-64-feat-add-elk-apm-configurations [skip ci]
    
    * Bump to version 1.5.0-PRDP-64-feat-add-elk-apm-configurations [skip ci]
    
    ---------
    
    Co-authored-by: acialini <ale.cialini@gmail.com>
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

--- src/main/java/it/gov/pagopa/project/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package it.gov.pagopa.project.util;
-
-public class Constants {
-
-    public static final String UNZIPPED_FILES_FOLDER = "/unzipped";
-    public static final String ZIP_FILE_NAME = "/input.zip";
-
-    private Constants() {}
-}
