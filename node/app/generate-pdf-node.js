bb68525c7c638d639974bc477f9b1c53870dd8b4
acialini <ale.cialini@gmail.com>
2023-08-24 00:03:03 +0200

    Node tests

--- node/app/generate-pdf-node.js
+++ node/app/generate-pdf-node.js
@@ -4,26 +4,30 @@ const { app } = require('@azure/functions');
 const puppeteer = require('puppeteer');
 const path = require('path');
 
-app.http('generatePdf', {
-    route: 'generate-pdf',
-    handler: async (request, context) => {
+module.exports = async function (context, request) {
+    let message = 'Hello, world!';
 
-        const transactionID = 'F57E2F8E-25FF-4183-AB7B-4A5EC1A96644';
+     const transactionID = 'F57E2F8E-25FF-4183-AB7B-4A5EC1A96644';
 
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
+              const browser = await puppeteer.launch();
+              const page = await browser.newPage();
 
-          const htmlFile = path.resolve('index.html');
+              const htmlFile = path.resolve('index.html');
 
-          await page.goto(`file://${htmlFile}`, { waitUntil: 'networkidle2' });
-          await page.pdf({
-            path: `pagopa-receipt-${transactionID}.pdf`,
-            format: 'A4',
-            landscape: false,
-            printBackground: true,
-          });
+              await page.goto(`file://${htmlFile}`, { waitUntil: 'networkidle2' });
+              await page.pdf({
+                path: `pagopa-receipt-${transactionID}.pdf`,
+                format: 'A4',
+                landscape: false,
+                printBackground: true,
+              });
 
-          await browser.close();
+              await browser.close();
 
-    }
-});
+    return {
+        httpResponse: {
+            body: message
+        },
+        queueOutput: message
+    };
+};
