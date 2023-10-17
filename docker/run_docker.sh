7ac5cbb868123ae4d55a46f7225e76f6c9637fce
acialini <alessio.cialini@nttdata.com>
2023-10-17 18:19:48 +0200

    quarkus_app_test: introducing reactive management

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -46,7 +46,7 @@ done
 
 
 stack_name=$(cd .. && basename "$PWD")
-docker build -t pagopa-pdf-engine-node -f ../Dockerfile_integration_test ../node
+docker build -t pagopa-pdf-engine-node ../node
 docker build -t pagopa-pdf-engine -f ../Dockerfile_integration_test ../
 docker run -d -p 3000:3000 --name="${stack_name}-node" --network=host pagopa-pdf-engine-node
 docker run -d -p 8080:80 --name="${stack_name}" --network=host pagopa-pdf-engine
