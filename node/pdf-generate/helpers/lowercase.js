aabc5ec3ad41a357a6916d9e53d8607f6c768c1e
Alessio Cialini <63233981+alessio-cialini@users.noreply.github.com>
2023-11-27 14:47:23 +0100

    fix: fixed lowercase function register (#92)
    Co-authored-by: acialini <ale.cialini@gmail.com>

--- node/pdf-generate/helpers/lowercase.js
+++ node/pdf-generate/helpers/lowercase.js
@@ -1,9 +1,5 @@
-var lowercase = function () {};
-
-lowercase.register = function (Handlebars) {
-  Handlebars.registerHelper("lowercase", function (str) {
+var lowercase = function (str) {
     return str.toLowerCase();
-  });
 };
 
 module.exports = lowercase;
