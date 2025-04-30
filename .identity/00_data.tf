2bd6e691621c95491447d67e2d5ce2ec28a1500d
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-04-30 10:41:40 +0200

    chore: update helm chart and github runner logic

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -61,3 +61,8 @@ data "azurerm_key_vault_secret" "key_vault_az_devops" {
   key_vault_id = data.azurerm_key_vault.key_vault_domain.id
 }
 
+data "azurerm_user_assigned_identity" "workload_identity_clientid" {
+  name                = "shared-workload-identity"
+  resource_group_name = "${local.product}-weu-${var.env}-aks-rg"
+}
+
