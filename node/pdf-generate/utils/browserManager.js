86a21cee003dd559f6efdecf6dadabed7802a77f
giomella <giomella@emeal.nttdata.com>
2026-03-09 12:45:07 +0100

    remove merge folders logic and update i18 initialization with folder logic

--- node/pdf-generate/utils/browserManager.js
+++ node/pdf-generate/utils/browserManager.js
@@ -1,81 +1,60 @@
 const puppeteer = require('puppeteer');
 let handlebars = require("handlebars");
-let { readFileSync, readdirSync } = require('fs');
+let { readFileSync, readdirSync, existsSync } = require('fs');
 const path = require('node:path');
+const HandlebarsI18n = require("handlebars-i18n");
+const i18next = require("i18next");
 
-const getDirectories = source => readdirSync(source, { withFileTypes: true })
-.filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
 const getFiles = source => readdirSync(source, { withFileTypes: true })
-.filter(dirent => !dirent.isDirectory())
+    .filter(dirent => !dirent.isDirectory())
     .map(dirent => dirent.name)
 const importFile = (filePath, fileName) => readFileSync(`${filePath}/${fileName}`, "utf8");
 const requireFile = (filePath, fileName) => require(`${filePath}/${fileName}`);
 
 const partialPath = `./pdf-generate/partials`;
 const helpersPath = `./pdf-generate/helpers`;
+const localesPath = `./pdf-generate/locales`;
+const envFolder = process.env.FOLDERS_TO_LOAD || "receipts";
+console.log(envFolder);
 
-let browser;
-
-const HandlebarsI18n = require("handlebars-i18n");
-
-const i18next = require("i18next");
-i18next.init({
-    resources: JSON.parse(readFileSync('./pdf-generate/assets/i18next.json', 'utf8')),
-    lng: "it"
-});
+const localeFilePath = `${localesPath}/${envFolder}/i18next.json`;
+if (existsSync(localeFilePath)) {
+    i18next.init({
+        resources: JSON.parse(readFileSync(localeFilePath, "utf8")),
+        lng: "it"
+    });
+} else {
+    console.warn(`File traduzioni non trovato: ${localeFilePath}`);
+}
 
 HandlebarsI18n.init();
 
+let browser;
 const getBrowserSession = async () => {
-  if (browser) return browser;
+    if (browser) return browser;
 
-  const envFolders = process.env.FOLDERS_TO_LOAD || "receipts";
-  let includedFolders = [];
-  if (envFolders) {
-          includedFolders = envFolders.split(',')
-          .map(folder => folder.trim())
-          .filter(folder => folder.length > 0);
-  }
-
-  const includeAll = includedFolders.length === 0;
-
-  console.log(includedFolders);
-
-  // Register helpers
- const allHelperDirectories = getDirectories(helpersPath);
-    const helperDirectoriesToLoad = includeAll
-        ? allHelperDirectories
-        : allHelperDirectories.filter(dir => includedFolders.includes(dir)); // Filtro
-  for (directoryHelper of helperDirectoriesToLoad) {
-    const directoryHelperFiles = getFiles(`${helpersPath}/${directoryHelper}`);
-    for (directoryHelperFile of directoryHelperFiles) {
-        const helper = requireFile(`../helpers/${directoryHelper}`, path.parse(`${helpersPath}/${directoryHelper}/${directoryHelperFile}`).name);
+    // Register helpers
+    const directoryHelperFiles = getFiles(`${helpersPath}/${envFolder}`);
+    for (const directoryHelperFile of directoryHelperFiles) {
+        const helper = requireFile(`../helpers/${envFolder}`, path.parse(`${helpersPath}/${envFolder}/${directoryHelperFile}`).name);
         handlebars.registerHelper(
-            path.parse(`${helpersPath}/${directoryHelper}/${directoryHelperFile}`).name , helper);
+            path.parse(`${helpersPath}/${envFolder}/${directoryHelperFile}`).name, helper);
     }
-  }
 
-  // Register partials
-  const allPartialDirectories = getDirectories(partialPath);
-    const partialDirectoriesToLoad = includeAll
-        ? allPartialDirectories
-        : allPartialDirectories.filter(dir => includedFolders.includes(dir)); // Filtro
-  for (directoryPartial of partialDirectoriesToLoad) {
-    const directoryPartialFiles = getFiles(`${partialPath}/${directoryPartial}`);
-    for (directoryPartialFile of directoryPartialFiles) {
-        const partial = importFile(`${partialPath}/${directoryPartial}`, directoryPartialFile);
+    // Register partials
+    const directoryPartialFiles = getFiles(`${partialPath}/${envFolder}`);
+    for (const directoryPartialFile of directoryPartialFiles) {
+        const partial = importFile(`${partialPath}/${envFolder}`, directoryPartialFile);
         handlebars.registerPartial(
-            path.parse(`${partialPath}/${directoryPartial}/${directoryPartialFile}`).name , partial);
+            path.parse(`${partialPath}/${envFolder}/${directoryPartialFile}`).name, partial);
     }
-  }
 
-  browser = await puppeteer.launch({
-    headless: true,
-    args: ["--headless","--no-sandbox", "--font-render-hinting=none"]
-  });
+    browser = await puppeteer.launch({
+        headless: true,
+        args: ["--headless", "--no-sandbox", "--font-render-hinting=none"]
+    });
 
-  return browser;
+    return browser;
 };
 
 const closeBrowserSession = async () => {
