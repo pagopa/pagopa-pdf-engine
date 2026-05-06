c1781a3cfa318f28804a7e675d3677748947ced0
giomella <giomella@emeal.nttdata.com>
2026-05-06 14:39:05 +0200

    [PIDM-1904] simplify run_docker.sh script

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -10,41 +10,6 @@ then
   echo "No environment specified: local is used."
 fi
 
-pip3 install yq
-
-if [ "$ENV" = "local" ]; then
-  image="service-local:latest"
-  ENV="dev"
-else
-  repository=$(yq -r '."microservice-chart".image.repository' ../helm/values-$ENV.yaml)
-  image="${repository}:latest"
-fi
-export image=${image}
-
-FILE=.env
-if test -f "$FILE"; then
-    rm .env
-fi
-config=$(yq  -r '."microservice-chart".envConfig' ../helm/values-$ENV.yaml)
-for line in $(echo $config | jq -r '. | to_entries[] | select(.key) | "\(.key)=\(.value)"'); do
-    echo $line >> .env
-done
-
-keyvault=$(yq  -r '."microservice-chart".keyvault.name' ../helm/values-$ENV.yaml)
-secret=$(yq  -r '."microservice-chart".envSecret' ../helm/values-$ENV.yaml)
-for line in $(echo $secret | jq -r '. | to_entries[] | select(.key) | "\(.key)=\(.value)"'); do
-  IFS='=' read -r -a array <<< "$line"
-  response=$(az keyvault secret show --vault-name $keyvault --name "${array[1]}")
-  value=$(echo $response | jq -r '.value')
-  echo "${array[0]}=$value" >> .env
-#  if [ "${array[0]}" = "AFM_SA_CONNECTION_STRING" ];then
-#      echo "Set secret env ${array[0]}"
-#      echo "::add-mask::$value"
-#      echo AFM_SA_CONNECTION_STRING=$value >> $GITHUB_ENV
-#  fi 
-done
-
-
 stack_name=$(cd .. && basename "$PWD")
 docker build -t pagopa-pdf-engine-node ../node
 docker build -t pagopa-pdf-engine ../
